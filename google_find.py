4c72779a6fff51fe568efd9d70a527b34fe7ca7a
ok <and2user@gmail.com>
2022-03-08 23:38:10 +0900

    second_commit

--- google_find.py
+++ google_find.py
@@ -1,17 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome import service as fs
+
+CHROMEDRIVER = r"C:\Users\pando\Desktop\python_automation\chromedriver.exe"
 
 options = webdriver.ChromeOptions()
 # options.add_argument("--headless")
 
-driver = webdriver.Chrome(r"C:\Users\pando\Desktop\python_automation\chromedriver.exe", options=options)
+chrome_service = fs.Service(executable_path= CHROMEDRIVER)
+driver = webdriver.Chrome(service= chrome_service)
 driver.implicitly_wait(10)
-driver.get("https://www.google.com")
+driver.get("https://www.google.com/")
 
-text_box = driver.find_element_by_name("q")
+text_box = driver.find_element(by=By.NAME, value="q")
 text_box.send_keys("株式会社ヘルスベイシス")
 text_box.submit()
 driver.implicitly_wait(10)
 
-
-address = driver.find_element_by_xpath("//div[@class='BOu6vf']/span")
+"//*[@class='LrzXr']/span"
+address = driver.find_element(by=By.CLASS_NAME, value="LrzXr")
 print(address.text)
